f68a817838b83969268a47b4313f1c5114144e25
Dan Parry <parryd424@gmail.com>
2017-11-18 10:37:05 -0500

    first commit and need for version control

---
+++ README.md
@@ -0,0 +1,11 @@
+This repository is a personal exercise in scraping data and writing into CSV files which will later be used to seed a database for the creation of an API.
+
+My first attempt at scraping was a [simple version](https://github.com/parryd4/simple-scraper-the-elite). This will access and gather more data. 
+
+# Disclaimer
+"The-elite.net is not in any way affiliated or involved with Rareware or Nintendo. Perfect Dark, GoldenEye 007, and all related material are registered trademarks of Rareware. All other content is copyright © the-elite.net 2002 - 2017.
+System developed free of charge by Ryan Dwyer."
+
+Data on The-elite.net is accessible without an account. The reason I started this scraping project was because I noticed the-elite was lacking an API, and I thought it would be a fun project to build one for them. I need data to mimic behavior and performance and so I shall scrape it.
+
+None of the data scraped is intended for ANY use other than my own learning experience and interest.
---
+++ lib/scraper.rb
@@ -0,0 +1,91 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+require 'CSV'
+
+module MyHelper
+  def time_to_s(time_string)
+    m_s = time_string.split(":").map(&:to_i)
+    m_s.length == 1 ? "N/A" : m_s[0]*60 + m_s[1]
+  end
+end
+
+class Scraper
+  include MyHelper
+  attr_accessor :names
+
+  def initialize
+    @names = []
+  end
+
+  def get_personal_records
+    CSV.open("./seeds/lb_records.csv","wb") do |csv|
+
+      player = "LimitBreakr"
+      # player = "Perfect+Ace"
+      doc = Nokogiri::HTML(open("https://rankings.the-elite.net/~#{player}/goldeneye/history"))
+      data = doc.css("td")
+
+      i = 0
+      while (i < data.length)
+        # binding.pry
+        csv << [
+          data[i].text, # date_achieved https://apidock.com/ruby/DateTime/strftime
+          data[i+1].text, # stage
+          data[i+2].text, # difficulty
+          time_to_s(data[i+3].text), # time_seconds, m:s to s
+          data[i+3].children.first.attributes.first.last.value.split("/").last.to_i, # personal_record_id
+          data[i+4].text, # system
+          data[i+5].text == "Yes", # current pr?
+          data[i+6].text == "Yes", # video?
+        ]
+        i += 7
+        # puts arr
+      end
+    end
+  end
+
+  def get_players
+    # top 50
+    doc = Nokogiri::HTML(open("https://rankings.the-elite.net/ajax/rankings/ge/"))
+
+    text = doc.css("p").children.text.split(":").last.split(",")
+    @names = text.values_at(* text.each_index.select{ |i| (i-1)%6 == 0})
+
+    # remaining names
+    doc = Nokogiri::HTML(open("https://rankings.the-elite.net/ajax/rankings/ge/post50/1510367957"))
+
+    text = doc.css("p").children.text.split(":").last.split(",")
+    remaining_names = text.values_at(* text.each_index.select{ |i| (i-1)%6 == 0})
+
+    @names << remaining_names
+    @names.flatten!
+
+    @names.map! { |n| n[1..-2]}
+
+    # binding.pry
+    CSV.open("./seeds/player_names.csv","wb") do |csv|
+      @names.each { |n| csv << [n] }
+    end
+
+  end
+
+  def get_player_achievements
+    CSV.open("./seeds/achievements.csv","wb") do |csv|
+      @names.each do |player|
+        doc = Nokogiri::HTML(open("https://rankings.the-elite.net/~#{player}/achievements"))
+        i = 0
+        while (i < doc.css(".achievement").length)
+          csv << [player, doc.css(".achievement-name")[i], doc.css(".achievement-description")[i]]
+          i += 1
+        end
+      end
+    end
+  end
+
+end
+
+sample_seed = Scraper.new
+# sample_seed.get_players
+# sample_seed.get_player_achievements
+sample_seed.get_personal_records
